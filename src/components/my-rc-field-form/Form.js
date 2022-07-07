0db12b5cf2abe3ac180aa23f542662856bb22614
Warn <imwarn@163.com>
2022-07-07 15:58:18 +0800

    修复⚠️

--- src/components/my-rc-field-form/Form.js
+++ src/components/my-rc-field-form/Form.js
@@ -1,8 +1,14 @@
+import React from 'react';
+
 import FieldContext from "./FieldContext";
 import useForm from "./useForm";
 
-export default function Form ({ children, form, onFinish, onFinishFailed }) {
+export default function Form ({ children, form, onFinish, onFinishFailed }, ref) {
   const [formInstance] = useForm(form);
+  React.useImperativeHandle(
+    ref,
+    () => formInstance
+  )
   formInstance.setCallBacks({onFinish, onFinishFailed});
   return (
     <form onSubmit={(e) => {
--- src/pages/MyRCFieldForm.js
+++ src/pages/MyRCFieldForm.js
@@ -6,67 +6,70 @@ import Input from "../components/Input";
 const nameRules = { required: true, message: "请输入姓名！" };
 const passwordRules = { required: true, message: "请输入密码！" };
 
-export default function MyRCFieldForm(props) {
-  const [form] = Form.useForm();
+// export default function MyRCFieldForm(props) {
+//   const [form] = Form.useForm();
 
-  const onFinish = (val) => {
-    console.log("onFinish", val);
-  };
-
-  const onFinishFailed = (val) => {
-    console.log("onFinishFailed", val);
-  };
-
-  useEffect(() => {
-    console.log("form", form);
-  }, []);
-
-  return (
-    <div>
-      <h3>MyRCFieldForm</h3>
-      <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
-        <Field name="username" label="姓名" rules={[nameRules]}>
-          <Input placeholder="please enter username" />
-        </Field>
-        <Field name="password" label="密码" rules={[passwordRules]}>
-          <Input placeholder="please enter password" />
-        </Field>
-        <button>Submit</button>
-      </Form>
-    </div>
-  );
-}
-
-// export default class MyRCFieldForm extends Component {
-//   formRef = React.createRef();
-//   componentDidMount() {
-//     console.log('form', this.formRef.current);
-//   }
-
-//   onFinish = (val) => {
+//   const onFinish = (val) => {
 //     console.log("onFinish", val);
 //   };
 
-//   onFinishFailed = (val) => {
+//   const onFinishFailed = (val) => {
 //     console.log("onFinishFailed", val);
 //   };
 
-  
-  
-//   render() {
-//     return (
-//       <div>
-//         <h3>MyRCFieldForm</h3>
-//         <Form ref={this.formRef} onFinish={this.onFinish} onFinishFailed={this.onFinishFailed}>
-//           <Field name="username" label="姓名" rules={[nameRules]}>
-//             <Input placeholder="please enter username" />
-//           </Field>
-//           <Field name="password" label="密码" rules={[passwordRules]}>
-//             <Input placeholder="please enter password" />
-//           </Field>
-//           <button>Submit</button>
-//         </Form>
-//       </div>
-//     );
-//   }
+//   useEffect(() => {
+//     console.log("form", form);
+//   }, []);
+
+//   return (
+//     <div>
+//       <h3>MyRCFieldForm</h3>
+//       <Form form={form} onFinish={onFinish} onFinishFailed={onFinishFailed}>
+//         <Field name="username" label="姓名" rules={[nameRules]}>
+//           <Input placeholder="please enter username" />
+//         </Field>
+//         <Field name="password" label="密码" rules={[passwordRules]}>
+//           <Input placeholder="please enter password" />
+//         </Field>
+//         <button>Submit</button>
+//       </Form>
+//     </div>
+//   );
 // }
+
+export default class MyRCFieldForm extends Component {
+  formRef = React.createRef();
+  componentDidMount() {
+    console.log("form", this.formRef.current);
+    this.formRef.current.setFieldsValue({ username: "default" });
+  }
+
+  onFinish = (val) => {
+    console.log("onFinish", val);
+  };
+
+  onFinishFailed = (val) => {
+    console.log("onFinishFailed", val);
+  };
+
+  render() {
+    return (
+      <div>
+        <h3>MyRCFieldForm</h3>
+        <Form
+          ref={this.formRef}
+          onFinish={this.onFinish}
+          onFinishFailed={this.onFinishFailed}
+        >
+          <Field name="username" label="姓名" rules={[nameRules]}>
+            <Input placeholder="please enter username" />
+          </Field>
+          <Field name="password" label="密码" rules={[passwordRules]}>
+            <Input placeholder="please enter password" />
+          </Field>
+          <button>Submit</button>
+        </Form>
+      </div>
+    );
+  }
+}
