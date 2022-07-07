3510a022bd0cff880de5cc18527b04e38c367e3e
Warn <imwarn@163.com>
2022-07-07 11:58:43 +0800

    before diy

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
-import AntdFormPage from './pages/AntdFormPage';
+// import AntdFormPage from './pages/AntdFormPage';
+import MyRCFieldForm from './pages/MyRCFieldForm';
 
 function App() {
   return (
-    <AntdFormPage />
+    // <AntdFormPage />
+    <MyRCFieldForm />
   );
 }
 
---
+++ src/components/my-rc-field-form/Form.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class Field extends Component {
+  render() {
+    return (
+      <div>
+        <h3>Form</h3>
+      </div>
+    )
+  }
+}
